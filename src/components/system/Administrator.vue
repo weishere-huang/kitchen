fa2848a2bc770627bfce43f85b092d0c069c6cd2
470538922 <470538922@qq.com>
2019-02-14 09:55:22 +0800

    添加管理员列表

---
+++ src/components/system/Administrator.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="administrator_list">
+    <div class="top_list">
+      <el-button
+        size="small"
+        type="primary"
+        class="el-icon-circle-plus-outline"
+      >添加商品</el-button>
+    </div>
+  </div>
+</template>
+<style lang="less">
+@main-color: #1cc09f;
+@bgColor: #f0f2f5;
+@font-normal: #333333;
+@font-subsidiary: #999999;
+@font-special: #1cc09f;
+@border: 1px solid #dde2eb;
+.administrator_list {
+  font-size: 14px;
+  color: @font-normal;
+  .top_list {
+    // line-height: 60px;
+    background-color: white;
+    padding: 10px;
+  }
+}
+</style>
