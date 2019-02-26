6cd6b41d0d2487ad82e0429e19ab209861d93106
470538922 <470538922@qq.com>
2019-02-26 10:11:07 +0800

    修改弹窗样式

--- src/components/message/Advertising.vue
+++ src/components/message/Advertising.vue
@@ -62,10 +62,21 @@
             <template slot-scope="scope">
 
               <span @click="mainPicShow=true"><img
-                :src="scope.row.mainPic"
-                alt=""
-                style="width:180px;height:60px;"
-              ></span>
+                  :src="scope.row.mainPic"
+                  alt=""
+                  style="width:180px;height:60px;"
+                ></span>
+              <el-dialog
+                :visible.sync="mainPicShow"
+                append-to-body
+                class="showPic"
+              >
+                <img
+                  width="100%"
+                  :src="scope.row.mainPic"
+                  alt=""
+                >
+              </el-dialog>
             </template>
           </el-table-column>
           <el-table-column
@@ -73,11 +84,22 @@
             min-width="200"
           >
             <template slot-scope="scope">
-              <span @click="contentShow"><img
-                :src="scope.row.content"
-                alt=""
-                style="width:180px;height:60px;"
-              ></span>
+              <span @click="contentShow=true"><img
+                  :src="scope.row.content"
+                  alt=""
+                  style="width:180px;height:60px;"
+                ></span>
+                <el-dialog
+                :visible.sync="contentShow"
+                append-to-body
+                class="showPic"
+              >
+                <img
+                  width="100%"
+                  :src="scope.row.content"
+                  alt=""
+                >
+              </el-dialog>
             </template>
           </el-table-column>
           <el-table-column
@@ -176,232 +198,236 @@
   </div>
 </template>
 <script>
-  import addAdvertising from "./advertisingAdd/AddAdvertising";
+import addAdvertising from "./advertisingAdd/AddAdvertising";
 
-  export default {
-    inject: ["reload"],
-    data() {
-      return {
-        contentShow:false,
-        mainPicShow:false,
-        editMsg: {
-          // title: "",
-          // pic: "",
-          // picContent: "",
-          // isShow: 0,
-          // startTime: {
-          //   date: "",
-          //   time: ""
-          // },
-          // endTime: {
-          //   date: "",
-          //   time: ""
-          // }
+export default {
+  inject: ["reload"],
+  data() {
+    return {
+      contentShow: false,
+      mainPicShow: false,
+      editMsg: {
+        // title: "",
+        // pic: "",
+        // picContent: "",
+        // isShow: 0,
+        // startTime: {
+        //   date: "",
+        //   time: ""
+        // },
+        // endTime: {
+        //   date: "",
+        //   time: ""
+        // }
+      },
+      addMsg: {
+        // title: "",
+        // pic: "",
+        // picContent: "",
+        // isShow: 0,
+        // startTime: {
+        //   date: "",
+        //   time: ""
+        // },
+        // endTime: {
+        //   date: "",
+        //   time: ""
+        // }
+      },
+      dialogEdit: false,
+      dialogAdd: false,
+      currentPage: 1,
+      tableData: [
+        {
+          title: "满99减50",
+          mainPic:
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550220559773&di=d5539846e2a20bf9bff4187b1f2e4ea5&imgtype=0&src=http%3A%2F%2Fpic34.nipic.com%2F20131031%2F9612701_091323702350_2.jpg",
+          picContent:
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550220559773&di=d5539846e2a20bf9bff4187b1f2e4ea5&imgtype=0&src=http%3A%2F%2Fpic34.nipic.com%2F20131031%2F9612701_091323702350_2.jpg",
+          startTime: "2018-09-01 00:00:00",
+          endTime: "2018-10-09 00:00:00",
+          linkCount: "88"
+        }
+      ],
+      pageIndex: 1,
+      pageSize: 10,
+      total: 100
+    };
+  },
+  methods: {
+    toadd() {
+      this.addMsg = {};
+      this.editMsg = {};
+      this.dialogAdd = true;
+    },
+    handleEdit(index, rowData) {
+      let params = { type: "edit", index: index, rowData: rowData };
+      this.editMsg = {};
+      this.addMsg = {};
+      Object.assign(this.editMsg, rowData);
+      console.log(params);
+      this.dialogEdit = true;
+    },
+    handleDelete(index, rowData) {
+      rowData.visible = false;
+      let params = { type: "delete", index: index, rowData: rowData };
+      this.beforeDelete(rowData.id);
+      console.log(params);
+    },
+    getAdvertisingList() {
+      this.Axios(
+        {
+          params: {},
+          option: {},
+          type: "get",
+          url: "/api-platform/Advertisement/list"
         },
-        addMsg: {
-          // title: "",
-          // pic: "",
-          // picContent: "",
-          // isShow: 0,
-          // startTime: {
-          //   date: "",
-          //   time: ""
-          // },
-          // endTime: {
-          //   date: "",
-          //   time: ""
-          // }
+        this
+      ).then(
+        result => {
+          console.log(result.data);
+          this.tableData = result.data.data;
         },
-        dialogEdit: false,
-        dialogAdd: false,
-        currentPage: 1,
-        tableData: [
-          {
-            title: "满99减50",
-            mainPic:
-              "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550220559773&di=d5539846e2a20bf9bff4187b1f2e4ea5&imgtype=0&src=http%3A%2F%2Fpic34.nipic.com%2F20131031%2F9612701_091323702350_2.jpg",
-            picContent:
-              "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550220559773&di=d5539846e2a20bf9bff4187b1f2e4ea5&imgtype=0&src=http%3A%2F%2Fpic34.nipic.com%2F20131031%2F9612701_091323702350_2.jpg",
-            startTime: "2018-09-01 00:00:00",
-            endTime: "2018-10-09 00:00:00",
-            linkCount: "88"
-          }
-        ],
-        pageIndex: 1,
-        pageSize: 10,
-        total: 100
-      };
+        ({ type, info }) => {}
+      );
     },
-    methods: {
-      toadd(){
-        this.addMsg={};
-        this.editMsg={};
-        this.dialogAdd = true
-      },
-      handleEdit(index, rowData) {
-        let params = {type: "edit", index: index, rowData: rowData};
-        this.editMsg={};
-        this.addMsg={};
-        Object.assign(this.editMsg,rowData);
-        console.log(params);
-        this.dialogEdit = true;
-      },
-      handleDelete(index, rowData) {
-        rowData.visible=false
-        let params = {type: "delete", index: index, rowData: rowData};
-        this.beforeDelete(rowData.id);
-        console.log(params);
-      },
-      getAdvertisingList() {
-        this.Axios(
-          {
-            params: {},
-            option: {},
-            type: "get",
-            url: "/api-platform/Advertisement/list"
-          },
-          this
-        ).then(
-          result => {
-            console.log(result.data);
-            this.tableData = result.data.data;
-
-          },
-          ({type, info}) => {
-          }
-        );
-      },
-      beforeDelete(id){
-        this.deleteAdvertising(id)
-      },
-      deleteAdvertising(id){
-        let qs = require("qs");
-        let data = qs.stringify({
-          id:id
-        });
-        this.Axios({
-          params:data,
-          url:"/api-platform/Advertisement/delete",
-          type:"post",
-          option:{
-          }
-        },this).then(result=>{
-          console.log(result.data);
-          if(result.data.code===200){
-            this.reload();
-          }
-        })
-      },
-      //添加广告
-      beforeadd(){
-        this.addAdvertising();
-      },
-      addAdvertising(){
-        let qs = require("qs");
-        let data = qs.stringify({
-          title:this.addMsg.title,
-          // mainPic:this.addMsg.mainPic,
-          mainPic:"test",
-          // content:this.addMsg.content,
-          content:"test",
-          advertType:0,
-          advertContentType:0,
-          startTime:this.addMsg.startTime,
-          endTime:this.addMsg.endTime,
-        });
-        this.Axios({
-          params:data,
-          url:"/api-platform/Advertisement/add",
-          type:"post",
-          option:{
-          }
-        },this).then(result=>{
-          console.log(result.data);
-          if(result.data.code===200){
-            this.reload();
-          }
-        })
-      },
-      //修改广告
-      beforeupdate(){
-        this.updateAdvertising();
-      },
-      updateAdvertising(){
-        let qs = require("qs");
-        let data = qs.stringify({
-          id:this.editMsg.id,
-          title:this.editMsg.title,
-          mainPic:this.editMsg.mainPic,
-          content:this.editMsg.content,
-          advertType:0,
-          advertContentType:0,
-          startTime:this.editMsg.startTime.replace(/-/g,"/"),
-          endTime:this.editMsg.endTime.replace(/-/g,"/"),
-        });
-        this.Axios({
-          params:data,
-          url:"/api-platform/Advertisement/update",
-          type:"post",
-          option:{
-          }
-        },this).then(result=>{
-          console.log(result.data);
-          if(result.data.code===200){
-            this.reload();
-          }
-        })
-      },
+    beforeDelete(id) {
+      this.deleteAdvertising(id);
     },
-    created() {
-      this.getAdvertisingList();
+    deleteAdvertising(id) {
+      let qs = require("qs");
+      let data = qs.stringify({
+        id: id
+      });
+      this.Axios(
+        {
+          params: data,
+          url: "/api-platform/Advertisement/delete",
+          type: "post",
+          option: {}
+        },
+        this
+      ).then(result => {
+        console.log(result.data);
+        if (result.data.code === 200) {
+          this.reload();
+        }
+      });
     },
-    components: {
-      addAdvertising
+    //添加广告
+    beforeadd() {
+      this.addAdvertising();
+    },
+    addAdvertising() {
+      let qs = require("qs");
+      let data = qs.stringify({
+        title: this.addMsg.title,
+        // mainPic:this.addMsg.mainPic,
+        mainPic: "test",
+        // content:this.addMsg.content,
+        content: "test",
+        advertType: 0,
+        advertContentType: 0,
+        startTime: this.addMsg.startTime,
+        endTime: this.addMsg.endTime
+      });
+      this.Axios(
+        {
+          params: data,
+          url: "/api-platform/Advertisement/add",
+          type: "post",
+          option: {}
+        },
+        this
+      ).then(result => {
+        console.log(result.data);
+        if (result.data.code === 200) {
+          this.reload();
+        }
+      });
+    },
+    //修改广告
+    beforeupdate() {
+      this.updateAdvertising();
+    },
+    updateAdvertising() {
+      let qs = require("qs");
+      let data = qs.stringify({
+        id: this.editMsg.id,
+        title: this.editMsg.title,
+        mainPic: this.editMsg.mainPic,
+        content: this.editMsg.content,
+        advertType: 0,
+        advertContentType: 0,
+        startTime: this.editMsg.startTime.replace(/-/g, "/"),
+        endTime: this.editMsg.endTime.replace(/-/g, "/")
+      });
+      this.Axios(
+        {
+          params: data,
+          url: "/api-platform/Advertisement/update",
+          type: "post",
+          option: {}
+        },
+        this
+      ).then(result => {
+        console.log(result.data);
+        if (result.data.code === 200) {
+          this.reload();
+        }
+      });
     }
-  };
+  },
+  created() {
+    this.getAdvertisingList();
+  },
+  components: {
+    addAdvertising
+  }
+};
 </script>
 <style lang="less">
-  @main-color: #1cc09f;
-  @bgColor: #f0f2f5;
-  @font-normal: #333333;
-  @font-subsidiary: #999999;
-  @font-special: #1cc09f;
-  @border: 1px solid #dde2eb;
-  .advertising_list {
-    font-size: 14px;
-    color: @font-normal;
-    .top_list {
-      // line-height: 60px;
-      background-color: white;
+@main-color: #1cc09f;
+@bgColor: #f0f2f5;
+@font-normal: #333333;
+@font-subsidiary: #999999;
+@font-special: #1cc09f;
+@border: 1px solid #dde2eb;
+.advertising_list {
+  font-size: 14px;
+  color: @font-normal;
+  .top_list {
+    // line-height: 60px;
+    background-color: white;
+    padding: 10px;
+  }
+  .bottom_list {
+    background-color: white;
+    margin-top: 10px;
+    padding-bottom: 10px;
+    overflow: hidden;
+    .top_title {
+      padding: 0 10px;
+      line-height: 60px;
+      overflow: hidden;
+      border-bottom: @border;
+      h4 {
+        float: left;
+      }
+      .top_search {
+        width: 400px;
+        float: right;
+      }
+    }
+    .table_list {
+      overflow: hidden;
       padding: 10px;
     }
-    .bottom_list {
-      background-color: white;
-      margin-top: 10px;
-      padding-bottom: 10px;
-      overflow: hidden;
-      .top_title {
-        padding: 0 10px;
-        line-height: 60px;
-        overflow: hidden;
-        border-bottom: @border;
-        h4 {
-          float: left;
-        }
-        .top_search {
-          width: 400px;
-          float: right;
-        }
-      }
-      .table_list {
-        overflow: hidden;
-        padding: 10px;
-      }
-      .hint {
-        line-height: 50px;
-        padding-left: 20px;
-        color: @font-subsidiary;
-      }
+    .hint {
+      line-height: 50px;
+      padding-left: 20px;
+      color: @font-subsidiary;
     }
   }
+}
 </style>
