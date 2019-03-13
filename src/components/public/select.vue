4c5c050d683a7d0d74f533385ffa1dfc15e41ac9
470538922 <470538922@qq.com>
2019-03-13 17:10:50 +0800

    添加菜谱分类列表、添加、修改、删除接口

--- src/components/public/select.vue
+++ src/components/public/select.vue
@@ -4,82 +4,77 @@
 			:data="data"
 			:props="defaultProps"
 			@node-click="handleNodeClick"
-			default-expand-all
 			:expand-on-click-node="false"
 		></el-tree>
+		<!-- default-expand-all -->
 	</div>
 </template>
 <script>
 export default {
 	data() {
 		return {
-			data: [
-				{
-					label: "一级 1",
-					children: [
-						{
-							label: "二级 1-1",
-							children: [
-								{
-									label: "三级 1-1-1"
-								}
-							]
-						}
-					]
-				},
-				{
-					label: "一级 2",
-					children: [
-						{
-							label: "二级 2-1",
-							children: [
-								{
-									label: "三级 2-1-1"
-								}
-							]
-						},
-						{
-							label: "二级 2-2",
-							children: [
-								{
-									label: "三级 2-2-1"
-								}
-							]
-						}
-					]
-				},
-				{
-					label: "一级 3",
-					children: [
-						{
-							label: "二级 3-1",
-							children: [
-								{
-									label: "三级 3-1-1"
-								}
-							]
-						},
-						{
-							label: "二级 3-2",
-							children: [
-								{
-									label: "三级 3-2-1"
-								}
-							]
-						}
-					]
-				}
-			],
+			data: [],
 			defaultProps: {
 				children: "children",
-				label: "label"
+				label: "cateName"
 			}
 		};
 	},
 	methods: {
 		handleNodeClick(data) {
 			this.$emit("handlechange", data);
+		},
+		getClassifyList() {
+			this.Axios(
+				{
+					params: {},
+					option: {
+						enableMsg: false
+					},
+					type: "get",
+					url: "/api-recipe/recipeCate/listCate"
+				},
+				this
+			).then(
+				result => {
+					if (result.data.code === 200) {
+						let arr = Math.min.apply(
+							null,
+							result.data.data.map(item => {
+								return item.parentNo;
+							})
+						);
+						this.data = this.filterArray(result.data.data, arr);
+						// if (this.data == "" || null ) {
+						this.data.unshift({
+							cateName: "顶级分类",
+							cateNo: 0
+						});
+						// }
+					}
+				},
+				({ type, info }) => {}
+			);
+		},
+		filterArray(data, parent) {
+			let vm = this;
+			var tree = [];
+			var temp;
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].parentNo == parent) {
+					var obj = data[i];
+					// temp = this.filterArray(data, data[i].cateNo);
+					// if (temp.length > 0) {
+					// 	obj.children = temp;
+					// }
+					tree.push(obj);
+				}
+			}
+			return tree;
 		}
+	},
+	created() {
+		this.getClassifyList();
 	}
 };
 </script>
@@ -87,6 +82,7 @@ export default {
 .tree_case {
 	.el-tree-node__content:hover {
 		background-color: #1cc09f;
+		color: white;
 	}
 }
 </style>
