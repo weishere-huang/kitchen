0e9629d8e124fd71afa6c937283e859a93c38a1b
470538922 <470538922@qq.com>
2019-03-22 16:14:51 +0800

    修改图标跳转

--- src/App.vue
+++ src/App.vue
@@ -362,36 +362,19 @@ export default {
 		},
 
 		pathto(a) {
-			console.log(
-				this.$router.app.$options.router.options.routes.find(item => {
-					return item.path == "/AdminOrder";
-				})
-			);
 			if (a === 0) {
-				if (
-					this.$router.app.$options.router.options.routes.find(item => {
-						return item.path == "/AdminOrder";
-					}).path === "/AdminOrder"
-				) {
+				if (JSON.parse(sessionStorage.getItem("user")).employeeType == 0) {
 					this.$router.push({ path: "/AdminOrder" });
 				} else if (
-					this.$router.app.$options.router.options.routes.find(item => {
-						return item.path == "/Order";
-					}).path === "/Order"
+					JSON.parse(sessionStorage.getItem("user")).employeeType == 1
 				) {
 					this.$router.push({ path: "/Order" });
 				}
 			} else if (a === 1) {
-				if (
-					this.$router.app.$options.router.options.routes.find(item => {
-						return item.path == "/AdminStore";
-					}).path === "/AdminStore"
-				) {
+				if (JSON.parse(sessionStorage.getItem("user")).employeeType == 0) {
 					this.$router.push({ path: "/AdminStore" });
 				} else if (
-					this.$router.app.$options.router.options.routes.find(item => {
-						return item.path == "/Store";
-					}).path === "/Store"
+					JSON.parse(sessionStorage.getItem("user")).employeeType == 1
 				) {
 					this.$router.push({ path: "/Store" });
 				}
