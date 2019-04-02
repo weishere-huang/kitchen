0552fd636a8163169e88466f72ba6f47f5780350
470538922 <470538922@qq.com>
2019-04-02 17:57:40 +0800

    添加管理员工作台接口

--- src/components/global/Global.vue
+++ src/components/global/Global.vue
@@ -1,10 +1,13 @@
 <script>
 const apiSrc = "http://192.168.1.104:8861";
 const apiImg = "http://192.168.1.104:8861";
-const versionNumber = "1.0.0.190329_alpha";
+const versionNumber = "1.0.0.190402_alpha";
 //测试环境地址
 // const apiSrc = "http://192.168.1.106:8861";
 // const apiImg = "http://192.168.1.106:8861";
+//外网地址
+// const apiSrc = "http://118.25.217.48:8861";
+// const apiImg = "http://118.25.217.48:8861";
 export default {
 	apiSrc,
 	apiImg,
